242c10a4d5caa31cc51932b938b33467bb12c6d8
haiakab <49064075+haiakab@users.noreply.github.com>
2021-06-06 20:33:31 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>insurance project</code>
+          <code>insurance project haia</code>
         </p>
         <a
           className="App-link"
