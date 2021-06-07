be3fed191318c8b255e5db64988c05b9f1322329
haiakab <haiakablan96@gmail.com>
2021-06-07 19:33:55 +0300

    disconnect DB

--- insurance_company_node/modules/Pages/Auth/Login-controller.js
+++ insurance_company_node/modules/Pages/Auth/Login-controller.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 var router = express.Router()
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
-var loginDB = require('./Login-DB');
+// var loginDB = require('./Login-DB');
 // define the home page route
 
 
@@ -12,22 +12,23 @@ router.get('/', (req, res) => {
 });
 router.post('/login', async (req, res) =>  {
   console.log(req.body);
-  let result = await getUserData(req.body);
+  let result=req.body;
+  // let result = await getUserData(req.body);
    res.send(result);
-  //  `Server: "I received your POST request. Your Name is:  ${result.name}, Your email is: ${result.email},Your Password is: ${result.password}"`:result
+    `Server: "I received your POST request. Your Name is:  ${result.name}, Your email is: ${result.email},Your Password is: ${result.password}"`;
 });
 
-let getUserData = async (userInfo) => {
-  console.log("USERS DATA : " + userInfo);
-  let info= await loginDB.loginDB(userInfo);
-  if(info.length > 0 ){
-    if(userInfo.password != info[0].password)
-       return("uncorrect password!");
-    return(info);
-  }
-  else
-    return("unavailable user");
-} 
+// let getUserData = async (userInfo) => {
+//   console.log("USERS DATA : " + userInfo);
+//   let info= await loginDB.loginDB(userInfo);
+//   if(info.length > 0 ){
+//     if(userInfo.password != info[0].password)
+//        return("uncorrect password!");
+//     return(info);
+//   }
+//   else
+//     return("unavailable user");
+// } 
 
   
 
