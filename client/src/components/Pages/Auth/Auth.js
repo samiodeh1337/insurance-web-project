276020e4c09ee0b31bb99388867b639a6054ebf7
samiodeh1337 <samiodeh1337@gmail.com>
2021-07-02 18:38:01 -0700

    add modal

--- client/src/components/Pages/Auth/Auth.js
+++ client/src/components/Pages/Auth/Auth.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
+import { Col, Row, Form, Card, Button, FormCheck, Container, InputGroup, Modal } from '@themesberg/react-bootstrap';
+import cookie from 'react-cookies'
+
+function Auth(props) {
 
 
-export class Auth extends Component {
-    state = {
-        response: '',
-        name: '',
-        email: '',
-        password: '',
-        responseToPost: '',
-    };
-    //   componentDidMount() {
-    //     this.callApi()
-    //       .then(res => this.setState({ response: res.express }))
-    //       .catch(err => console.log(err));
-    //   }
-    //   callApi = async () => {
-    //     const response = await fetch('/Auth');
-    //     const body = await response.json();
-    //     if (response.status !== 200) throw Error(body.message);
-    //     return body;
-    //   };
-    handleSubmit = async e => {
-        e.preventDefault();
+
+    const [showDefault, setShowDefault] = useState(false);
+    const [message, setMessage] = useState('Error');
+
+    const handleClose = () => setShowDefault(false);
+
+    const handleSubmit = async e => {
+
+        setShowDefault(true)
+        /*e.preventDefault();
         const response = await fetch('/Auth/login', {
             method: 'POST',
             headers: {
@@ -30,7 +23,7 @@ export class Auth extends Component {
             body: JSON.stringify({ password: this.state.password, email: this.state.email, name: this.state.name }),
         });
         const body = await response.text();
-        this.setState({ responseToPost: body });
+        this.setState({ responseToPost: body });*/
     };
 
 
@@ -41,38 +34,53 @@ export class Auth extends Component {
     // );
 
 
-
-    render() {
-        return (
-            <div className="container" style={{ marginTop: '10%' }}>
-                <div className="row">
-                    <div className="col-md-4 py-5 bg-primary">
-                        <div className="card-body">
-                            <img src="http://www.ansonika.com/mavia/img/registration_bg.svg" style={{ width: '30%' }} />
-                            <h2 className="py-3">Login</h2>
-                        </div>
-                    </div>
-                    <div className="col-md-8 py-5 border">
-                        <h4 className="pb-4">Please fill with your details</h4>
-                        <form>
-
-                            <div className="form-group col-md-12">
-                                <input type="email" className="form-control" id="inputEmail4" placeholder="Email Adress" />
-                            </div>
-                            <div className="form-group col-md-12">
-                                <input type="email" className="form-control" id="inputEmail4" placeholder="Password" />
-                            </div>
-
-
-                            <div className="form-row">
-                                <button type="button" className="btn btn-primary col-md-12">Login</button>
-                            </div>
-                        </form>
+    return (
+        <div className="container" style={{ marginTop: '10%' }}>
+            <div className="row">
+                <div className="col-md-4 py-5 bg-primary">
+                    <div className="card-body">
+                        <img src="http://www.ansonika.com/mavia/img/registration_bg.svg" style={{ width: '30%' }} />
+                        <h2 className="py-3">Login</h2>
                     </div>
                 </div>
-            </div >
-        )
-    }
+                <div className="col-md-8 py-5 border">
+                    <h4 className="pb-4">Please fill with your details</h4>
+                    <form>
+
+                        <div className="form-group col-md-12">
+                            <input type="email" className="form-control" id="inputEmail4" placeholder="Email Adress" />
+                        </div>
+                        <div className="form-group col-md-12">
+                            <input type="email" className="form-control" id="inputEmail4" placeholder="Password" />
+                        </div>
+
+
+                        <div className="form-row">
+                            <button type="button" onClick={() => { handleSubmit() }} className="btn btn-primary col-md-12">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <React.Fragment>
+                <Modal as={Modal.Dialog} centered show={showDefault} onHide={handleClose}>
+                    <Modal.Header>
+                        <Modal.Title className="h6">Message</Modal.Title>
+                        <Button variant="close" aria-label="Close" onClick={handleClose} />
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>{message}</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            I Got It
+                        </Button>
+
+                    </Modal.Footer>
+                </Modal>
+            </React.Fragment>
+        </div >
+    )
+
 }
 // document.getElementById('.img-btn').addEventListener('click', function()
 // 	{
